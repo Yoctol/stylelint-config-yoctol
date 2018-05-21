a810af67024855829e8fc0aa9a4354676e2ae76d
Homer Chen <xxhomey19@gmail.com>
2018-05-21 17:20:04 +0800

    Use css-property-sort-order-smacss (#79)

--- index.js
+++ index.js
@@ -1,97 +1,18 @@
+const order = require('./order');
+
 module.exports = {
   extends: 'stylelint-config-standard',
   plugins: 'stylelint-order',
   rules: {
     'order/properties-order': [
-      // based on https://css-tricks.com/poll-results-how-do-you-order-your-css-properties/
       [
-        /* Positioning */
-        'position',
-        'z-index',
-        'top',
-        'right',
-        'bottom',
-        'left',
-
-        /* Display & Box Model */
-        'display',
-        'overflow',
-        'overflow-x',
-        'overflow-y',
-        'box-sizing',
-        'width',
-        'max-width',
-        'min-width',
-        'height',
-        'max-height',
-        'min-height',
-        'margin',
-        'margin-top',
-        'margin-right',
-        'margin-bottom',
-        'margin-left',
-        'padding',
-        'padding-top',
-        'padding-right',
-        'padding-bottom',
-        'padding-left',
-        'border',
-        'border-top',
-        'border-right',
-        'border-bottom',
-        'border-left',
-        'border-radius',
-        'border-top-right-radius',
-        'border-bottom-right-radius',
-        'border-bottom-left-radius',
-        'border-top-left-radius',
-        'outline',
-
-        /* Flex Container(Parent) */
-        'flex-flow',
-        'flex-direction',
-        'flex-wrap',
-        'justify-content',
-        'align-items',
-        'align-content',
-
-        /* Flex Item(Children) */
-        'flex',
-        'flex-grow',
-        'flex-shrink',
-        'flex-basis',
-        'order',
-        'align-self',
-
-        /* Color */
-        'background',
-        'background-image',
-        'color',
-        'box-shadow',
-
-        /* Text */
-        'font-family',
-        'font-size',
-        'line-height',
-        'text-align',
-        'text-decoration',
-        'text-shadow',
-        'vertical-align',
-        'white-space',
-
-        /* Effect */
-        'transition',
-        'transform',
-        'opacity',
-        'visibility',
-        'appearance',
-        'user-select',
-
-        /* Other */
-        'word-break',
-        'word-wrap',
-        'content',
-        'cursor',
+        ...order.heading,
+        ...order.box,
+        ...order.border,
+        ...order.background,
+        ...order.text,
+        ...order.animation,
+        ...order.other,
       ],
       { unspecified: 'bottomAlphabetical' },
     ],
